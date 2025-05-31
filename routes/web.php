5218eb3ef7a81f04e1c6749764300562255adbb8
Hendro Wibowo <hendrothemail@gmail.com>
2025-05-31 20:45:52 +0700

    feat: add patient management functionality
    - Add Patient model with necessary attributes
    - Create patients table migration
    - Implement PatientController with index method
    - Add patients index view
    - Update routes for patient management

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PatientController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// BUG 1: method create should be GET, store should be POST (swapped)
+Route::post('/patients/create', [PatientController::class, 'create'])->name('patients.create');
+Route::get('/patients', [PatientController::class, 'store'])->name('patients.store');
